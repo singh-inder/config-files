f2bade77a7132fdfe4e8424feb3f9892eaba9771
Inder Singh <85822513+singh-inder@users.noreply.github.com>
2024-12-04 00:57:29 +0530

    feat(helix/setup.sh): download common lang. servers

--- helix/setup.sh
+++ helix/setup.sh
@@ -1,5 +1,7 @@
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
   exit
 fi
 
@@ -7,4 +9,15 @@ add-apt-repository ppa:maveonair/helix-editor
 apt update
 apt install helix
 
-# TODO Install common lsp
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+if [ -x "$(command -v nvm)" ]; then
+  echo "Error installing nvm"
+  exit 1
+fi
+
+npm install -g dockerfile-language-server-nodejs @microsoft/compose-language-service yaml-language-server vscode-langservers-extracted typescript typescript-language-server
+
+for file in "languages.toml" "config.toml"; do
+  curl "https://raw.githubusercontent.com/singh-inder/config-files/refs/heads/main/helix/$file" -o ~/.config/helix/"$file"
+done
