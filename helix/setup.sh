6afd8832fd1a0350203f2888f5c6af43b6a13088
Inder Singh <85822513+singh-inder@users.noreply.github.com>
2024-12-04 14:19:46 +0530

    Update setup.sh

--- helix/setup.sh
+++ helix/setup.sh
@@ -27,7 +27,7 @@ nvm install --lts || exit 1
 
 npm install -g dockerfile-language-server-nodejs @microsoft/compose-language-service yaml-language-server vscode-langservers-extracted typescript typescript-language-server
 
-if [! -d ~/.config/helix ]; then
+if [ ! -d ~/.config/helix ]; then
   mkdir -p ~/.config/helix
 fi
 
