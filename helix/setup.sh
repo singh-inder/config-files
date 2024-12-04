19df3fc98c122f988395bd0272493385ff0f8b50
Inder Singh <85822513+singh-inder@users.noreply.github.com>
2024-12-04 14:01:49 +0530

    Update setup.sh

--- helix/setup.sh
+++ helix/setup.sh
@@ -14,8 +14,9 @@ fi
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-# reload .bashrc file to for nvm to work
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 if [ -x "$(command -v nvm)" ]; then
   echo "Error installing nvm"
