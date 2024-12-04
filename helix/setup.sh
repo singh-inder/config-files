9ccb575082ece39f303f19ee2b0f6341500f34f7
Inder Singh <85822513+singh-inder@users.noreply.github.com>
2024-12-04 14:46:02 +0530

    Update setup.sh

--- helix/setup.sh
+++ helix/setup.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
-fi
-
-add-apt-repository ppa:maveonair/helix-editor && apt update && apt install helix
+sudo add-apt-repository ppa:maveonair/helix-editor && sudo apt update && sudo apt install helix
 
 if [ $? -ne 0 ]; then
-  echo "Unable to install helix"
-  exit 1
+    echo "Unable to install helix"
+    exit 1
 fi
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 if [ -x "$(command -v nvm)" ]; then
-  echo "Error installing nvm"
-  exit 1
+    echo "Error installing nvm"
+    exit 1
 fi
 
 nvm install --lts || exit 1
@@ -28,10 +23,9 @@ nvm install --lts || exit 1
 npm install -g dockerfile-language-server-nodejs @microsoft/compose-language-service yaml-language-server vscode-langservers-extracted typescript typescript-language-server
 
 if [ ! -d ~/.config/helix ]; then
-  mkdir -p ~/.config/helix
+    mkdir -p ~/.config/helix
 fi
 
-
 for file in "languages.toml" "config.toml"; do
-  curl "https://raw.githubusercontent.com/singh-inder/config-files/refs/heads/main/helix/$file" -o ~/.config/helix/"$file"
+    curl "https://raw.githubusercontent.com/singh-inder/config-files/refs/heads/main/helix/$file" -o ~/.config/helix/"$file"
 done
